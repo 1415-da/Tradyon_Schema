474eb1e90a88cef892d9097447b2ca0c2148382f
Kumar Gaurav <kuga23cs@cmrit.ac.in>
2025-11-16 02:27:57 +0530

    Delete Schema/tradyon_schema/tradyon_schema directory

--- Schema/tradyon_schema/tradyon_schema/app/products/configure/page.tsx
+++
@@ -1,9 +0,0 @@
-import BulkProductConfigurator from '@/components/BulkProductConfigurator';
-
-export default function ConfigurePage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <BulkProductConfigurator />
-    </div>
-  );
-}
--- Schema/tradyon_schema/tradyon_schema/app/products/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductSelector from '@/components/ProductSelector';
-
-export default function ProductsPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <ProductSelector />
-    </div>
-  );
-}
